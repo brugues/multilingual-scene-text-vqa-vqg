064d650b531acb8b17a408250f818b2821b35847
Josep Brugués <josep.brugues@e-campus.uab.cat>
2021-06-03 19:29:43 +0200

    add evaluation step and evaluation data generator

--- src/train.py
+++ src/train.py
@@ -4,7 +4,8 @@ import tensorflow as tf
 
 from config.config import Config
 from models.stvqa import VQAModel
-from utils.utils import print_info, print_ok, update_progress_bar
+from eval import evaluate_batch
+from utils.utils import print_info, print_ok, update_train_progress_bar
 from utils.data_loader import STVQADataGenerator
 
 
@@ -18,43 +19,46 @@ if __name__ == '__main__':
     stvqa_model.attention_model.keras_model.summary()
     print_ok('Done!\n')
 
-    print_info('')
-    data_generator = STVQADataGenerator(config, True)
+    print_info('Preparing data generator')
+    train_data_generator = STVQADataGenerator(config)
+    eval_data_generator = STVQADataGenerator(config, training=False)
     print_ok('Done!\n')
 
     print_info('Starting training \n')
 
     step = 0
-    num_batches = data_generator.len()
+    num_batches = train_data_generator.len()
 
     for epoch in range(config.n_epochs):
         print_ok('Starting epoch number {}\n'.format(epoch))
 
         progress_bar = tqdm(range(num_batches),
-                            total=num_batches, desc='Training')
+                            total=num_batches, desc='Training Progress')
 
         for batch in range(num_batches):
             # Get data for this batch
-            batch_data = data_generator.next()
+            batch_data = train_data_generator.next()
 
             # Extract image features from Yolo
             img_features = stvqa_model.yolo_model.predict_on_batch(np.array(batch_data[0]))
             img_features = tf.cast(img_features, tf.float64)
 
-            image_emb = tf.concat([img_features, batch_data[1]], 3)
-
             # Train step
             loss = stvqa_model.attention_train_step(img_features, batch_data[1], batch_data[2], batch_data[3])
 
             if batch % config.logging_period == 0:
+                _, val_loss = evaluate_batch(stvqa_model, eval_data_generator.next())
                 stvqa_model.log_to_tensorboard(step, loss)
+                stvqa_model.log_to_tensorboard(step, val_loss, task='val')
 
             if batch % config.checkpoint_period == 0:
                 stvqa_model.save_attention_checkpoint()
 
-            update_progress_bar(progress_bar, epoch, config.n_epochs, batch, num_batches, loss)
+            update_train_progress_bar(progress_bar, epoch, config.n_epochs, batch, num_batches, loss)
 
             step += 1
 
         stvqa_model.save_attention_checkpoint()
         stvqa_model.save_attention_model()
+        tf.keras.backend.clear_session()  # Just in case there a memory leak or sth
+        del progress_bar
