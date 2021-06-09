6cd32504ab45047c8fe9e890d1b0ec31c73c9f05
jbrugues <bruguesjosep@protonmail.com>
2021-06-09 16:34:04 +0200

    change job file to avoid errors with paths

--- src/baseline.sh
+++ src/baseline.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH -n 4 # Number of cores
-#SBATCH --mem 16384 # 16GB.
-#SBATCH -D /home/jbrugues/multilingual-scene-text-vqa/src/outputs/slurm
-#SBATCH -p mhigh,mlow # or mlow Partition to submit to master low prioriy queue
-#SBATCH -q masterlow
+#SBATCH -N 1
+#SBATCH -D /home/jbrugues/multilingual-scene-text-vqa/src/
+#SBATCH -p dcc # or mlow Partition to submit to master low prioriy queue
+#SBATCH --mem 16384
 #SBATCH --gres gpu:Pascal:1 # Para pedir Pascales MAX 8
 #SBATCH -o %x_%u_%j.out # File to which STDOUT will be written
 #SBATCH -e %x_%u_%j.err # File to which STDERR will be written
-
 python train.py
