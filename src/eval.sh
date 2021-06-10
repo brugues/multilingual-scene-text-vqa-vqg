2d55e7e1e056e88c7452110d93f849489499694a
Josep Brugués <josep.brugues@e-campus.uab.cat>
2021-06-10 16:14:35 +0200

    add eval launcher job

---
+++ src/eval.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -n 4 # Number of cores
+#SBATCH -N 1
+#SBATCH -D /home/jbrugues/multilingual-scene-text-vqa/src/
+#SBATCH -p dcc # or mlow Partition to submit to master low prioriy queue
+#SBATCH --mem 16384
+#SBATCH --gres gpu:Pascal:1 # Para pedir Pascales MAX 8
+#SBATCH -o %x_%u_%j.out # File to which STDOUT will be written
+#SBATCH -e %x_%u_%j.err # File to which STDERR will be written
+python eval.py
